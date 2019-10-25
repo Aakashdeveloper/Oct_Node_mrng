dceab56e0ed07671bf33208ff62c4f139527c455
aakashDeveloper <ahanda205@gmail.com>
2019-10-25 03:13:42 +0100

    a

--- inbuilt/server.js
+++ inbuilt/server.js
@@ -1,9 +1,15 @@
 var http = require('http');
 var os = require('os');
 var port = 2400;
+var fs = require('fs');
 
 var server = http.createServer(function(req,res){
-    res.write("<h1>Hi "+os.userInfo().username+" you are using "+os.platform)
+    fs.readFile('db.json','utf-8',function(err,data){
+        if(err) throw err;
+        res.write(data);
+        res.end();
+    })
+    //res.write("<h1>Hi "+os.userInfo().username+" you are using "+os.platform)
     //res.write("<h1> This is my first node Server</h1>")
 });
 
